1def89885a086531812b25e1fd57b2e4890b6de7
Shubham Mishra <shubhammishra10@gmail.com>
2021-02-04 19:40:06 +0530

    second commit

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -29,7 +29,7 @@ struct CalculatorBrain {
         let bmiValue = weight / (height * height)
 
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Eat more donuts!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
         } else if bmiValue < 24.9 {
             bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
         } else {
